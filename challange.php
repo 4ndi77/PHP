65724efaa5bf4e3699c066af3930582777845c95
4ndi77 <andivejseli10@gmail.com>
2025-05-05 11:59:54 +0200

    ads

---
+++ challange.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>This is my first PHP challange</h1>
+        <h2>this is h2<h2>
+            <?php
+            echo"This is called an echo";
+            ?>
+    </body>
+</html>
