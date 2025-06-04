14b463c4bedce012b0e20485d298b8b5d8572ee6
4ndi77 <andivejseli10@gmail.com>
2025-06-04 12:11:40 +0200

    adsa

---
+++ lesson 9/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<form action="add.php" method="POST">
+    <input type="text" name="name" placeholder="Name"><br>
+    <input type="text" name="username" placeholder="Username"><br>
+    <input type="email" name="email" placeholder="Email"><br>
+    <button type="submit" name="submit">ADD</button>
+</form>
+
+
+
+
+</body>
+
+
+</html>
